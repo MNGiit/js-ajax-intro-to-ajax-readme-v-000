00f2ec6cb8637847e3426ab091bdc7fb79df052d
Mohammed Noori <kmanoorib@gmail.com>
2019-03-25 00:18:31 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -6,15 +6,14 @@ function getRepositories() {
 }
 
 function showRepositories() {
-  console.log(this.responseText);
-  let repoList = '<ul>';
-  for (var i = 0; i < this.responseText.length; i++) {
-    repoList += '<li>' + this.responseText[i]['name'] + '</li>';
-  }
-  repoList += '</ul>';
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos
+    .map(r => '<li>' + r.name + '</li>')
+    .join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
 }
- 
+
 function getRepositories() {
   const req = new XMLHttpRequest();
   req.addEventListener('load', showRepositories);
