32c76d349141c3d5b49956a0c2fdc655137c48af
Mohammed Noori <kmanoorib@gmail.com>
2019-03-25 00:19:07 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -21,9 +21,10 @@ function showRepositories() {
   document.getElementById('repositories').innerHTML = repoList;
 }
 
-function getRepositories() {
+function getCommits(el) {
+  const name = el.dataset.repo;
   const req = new XMLHttpRequest();
-  req.addEventListener('load', showRepositories);
-  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.addEventListener('load', showCommits);
+  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
   req.send();
 }
